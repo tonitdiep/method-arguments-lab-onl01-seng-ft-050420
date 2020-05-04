af04499a6dc672aaa0d632ac302dbe5c49d1b9c8
Toni  Diep <adiep123@gmail.com>
2020-05-04 19:23:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,10 @@ def introduction(name)
 end
 
 
+<<<<<<< HEAD
 def introduction_with_language(name, language)
+=======
+def introduction_with_langugage(name, language)
+>>>>>>> 17e3527a9de4fb195570d2113a052abddf1c95db
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
